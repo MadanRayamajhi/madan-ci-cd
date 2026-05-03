843fde4c71c09f6ba600be2837f7515e5e9f2545
Madan Rayamajhi <rayamajhimadan33@gmail.com>
2026-05-03 06:35:21 +0000

    update output message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🔥 CLEAN CI/CD PIPELINE - madan 🚀");
+  res.send(" CLEAN CI/CD PIPELINE - madan ");
 });
 
 app.listen(3000, () => {
